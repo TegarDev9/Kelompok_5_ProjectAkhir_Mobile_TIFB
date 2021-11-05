ddcb2fbca98681744726a639e8db4abd7cbfd0f3
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2021-11-05 13:40:01 +0700

    Desain login register

---
+++ Risqunaridho/app/src/main/java/com/risquna/risqunaridho/Login.java
@@ -0,0 +1,14 @@
+package com.risquna.risqunaridho;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_login );
+    }
+}
---
+++ Risqunaridho/app/src/main/java/com/risquna/risqunaridho/registrasi.java
@@ -0,0 +1,11 @@
+package com.risquna.risqunaridho;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class registrasi extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+    }
+}
