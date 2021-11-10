a2e114bc0255c8c3d8a63c037e25e7d720a7b2b3
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2021-11-10 07:55:55 +0700

    update

--- Risqunaridho/app/src/main/java/com/risquna/risqunaridho/Login.java
+++ Risqunaridho/app/src/main/java/com/risquna/risqunaridho/Login.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +16,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private Button btn_signin;
     EditText et_emailorphone, et_password;
-    Button btn_signin;
-    DBHelper dbHelper;
+
 
 
     @Override
@@ -31,45 +30,19 @@ public class Login extends AppCompatActivity {
         et_password = (EditText)findViewById(R.id.et_password);
         btn_signin = (Button)findViewById(R.id.btn_signin);
 
-        dbHelper = new DBHelper(this);
-        TextView tvCreateAccount = (TextView)findViewById(R.id.sigup);
 
-        tvCreateAccount.setText(fromHtml("I don't have account yet. " +
-                "</font><font color='#3b5998'>create one</font>"));
 
-        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this, registrasi.class));
-            }
-        });
-
+        btn_signin = findViewById(R.id.btn_signin);
         btn_signin.setOnClickListener(new View.OnClickListener() {
             @Override
+            /**Membuat inten pindah */
             public void onClick(View v) {
-                String email = et_emailorphone.getText().toString().trim();
-                String password = et_password.getText().toString().trim();
-
-                Boolean res = dbHelper.checkUser(email,password);
-                if(res == true){
-                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(Login.this, Menu.class));
-                }else {
-                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                }
+                Intent pindah = new Intent(Login.this, registrasi.class);
+                startActivity(pindah);
             }
         });
-    }
 
-    public static Spanned fromHtml(String html){
-        Spanned result;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
-        }else {
-            result = Html.fromHtml(html);
-        }
-        return result;
+
     }
 }
 
