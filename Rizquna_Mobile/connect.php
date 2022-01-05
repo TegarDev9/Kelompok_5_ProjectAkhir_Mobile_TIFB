f3764f9dcc89f5b3fd06022f668d9a07854b37cc
Tegar karunia ilham_E41200988 <86245238+TegarDev9@users.noreply.github.com>
2022-01-05 13:54:11 +0700

    update

--- Rizquna_Mobile/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('host', 'localhost');
-define('user', 'root');
-define('pass', '');
-define('db', 'risquna');
-
-$conn = mysqli_connect(host, user, pass, db) or die('Unable to Connect');
-?>
